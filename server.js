45584d446731fd82b619381eb4fe6181982078bb
nmudd037 <nithishreddymuddasani.7@gmail.com>
2020-11-12 04:19:44 -0500

    Changing PORT configuration

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose
   });
 
 //4) Start Server
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
