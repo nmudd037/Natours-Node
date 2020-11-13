008342eb9aa0a357bb750f22dac780c87c966db0
nmudd037 <nithishreddymuddasani.7@gmail.com>
2020-11-13 04:10:34 -0500

    Implementing CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //1) Global Middleware
+
+// Implement CORS
+app.use(cors()); //Sets Access-Control-Allow-Origin: *
+
+//api.natours.com, front-end: natours.com
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com'
+//   })
+// );
+
+app.options('*', cors());
+//app.options('/api/v1/tours/:id', cors());
 
 //Serving Static Files
 //app.use(express.static(`${__dirname}/public`));
