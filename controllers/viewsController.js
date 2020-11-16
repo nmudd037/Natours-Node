7e57a9dcbc05e44061c193bf2bc16b2f748b8cc5
nmudd037 <nithishreddymuddasani.7@gmail.com>
2020-11-16 00:36:29 -0500

    Improved Alerts

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -12,7 +12,7 @@ exports.alerts = (res, req, next) => {
       "Your booking was successfull! Please check your email for a confirmation. If your booking doesn't show up immediately, please come back later.";
   }
 
-  res.locals.alert = alert;
+  res.locals.alert = req.query ? alert : '';
   next();
 };
 
