4c0d5fb3f0afa1427155530aa94a1842664f32a8
nmudd037 <nithishreddymuddasani.7@gmail.com>
2020-11-16 01:19:17 -0500

    Improved Alerts

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -5,11 +5,14 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.alerts = (res, req, next) => {
-  const alert = req.query ? req.query.alert : '';
+  if (req.query.alert) {
+    const { alert } = req.query;
+    if (alert === 'booking') {
+      res.locals.alert =
+        "Your booking was successfull! Please check your email for a confirmation. If your booking doesn't show up immediately, please come back later.";
+    }
 
-  if (alert === 'booking') {
-    res.locals.alert =
-      "Your booking was successfull! Please check your email for a confirmation. If your booking doesn't show up immediately, please come back later.";
+    return next();
   }
 
   next();
